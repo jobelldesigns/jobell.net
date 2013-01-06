5c4cfeba797b7a3faf8ab6472772e4c1745b614f
Matt Heath <matt@mattheath.com>
2013-01-06 17:32:48 +0000

    Add deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,36 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "Jo Bell"
+set :repository,  "git@github.com:mattheath/jobell.net.git"
+set :scm, :git
+default_run_options[:pty] = true
+set :deploy_to, "/var/www/vhosts/jobell.net"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# use our keys, make sure we grab submodules, try to keep a remote cache
+ssh_options[:forward_agent] = true
+set :git_enable_submodules, 1
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# Clean up old releases
+set :keep_releases, 5
+after "deploy:update", "deploy:cleanup"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+# Define server
+role :web, "deploy@mattheath.com"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+namespace :deploy do
+  desc "Restart web server (reload apache)"
+  task :restart do
+    # Reload apache config
+    run "sudo /etc/init.d/httpd reload"
+  end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+  desc "Setup folder structure"
+  task :setup, :except => { :no_release => true } do
+    # Create folder structure for cap
+    dirs = [deploy_to, releases_path, shared_path]
+    dirs += shared_children.map { |d| File.join(shared_path, d.split('/').last) }
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')}"
+    run "#{try_sudo} chmod g+w #{dirs.join(' ')}" if fetch(:group_writable, true)
+  end
+end
+
